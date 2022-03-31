f534bfef7f78c194ab35e915b2be2f9192be2b92
DILIVEGX <diegoeugenio20@outlook.com>
2022-03-30 21:12:26 -0600

    builder

---
+++ generator-newbuild.js
@@ -0,0 +1,121 @@
+document.addEventListener("DOMContentLoaded", function(){
+    for(var i=0;i<30;i++){
+        document.getElementById("cpu").innerHTML+=`<div class="item">
+        <img src="assets/cpu.png" alt="">
+        <p class="name">Nombre del artículo</p>
+        <p class="price">$####</p>
+        <div class="rate">
+            <img src="assets/star.svg" alt="">
+            <img src="assets/star.svg" alt="">
+            <img src="assets/star.svg" alt="">
+            <img src="assets/star.svg" alt="">
+            <img src="assets/star.svg" alt="">
+        </div>
+    </div>`
+
+    document.getElementById("mb").innerHTML+=`<div class="item">
+        <img src="assets/mb.png" alt="">
+        <p class="name">Nombre del artículo</p>
+        <p class="price">$####</p>
+        <div class="rate">
+            <img src="assets/star.svg" alt="">
+            <img src="assets/star.svg" alt="">
+            <img src="assets/star.svg" alt="">
+            <img src="assets/star.svg" alt="">
+            <img src="assets/star.svg" alt="">
+        </div>
+    </div>`
+
+    document.getElementById("case").innerHTML+=`<div class="item">
+        <img src="assets/case.png" alt="">
+        <p class="name">Nombre del artículo</p>
+        <p class="price">$####</p>
+        <div class="rate">
+            <img src="assets/star.svg" alt="">
+            <img src="assets/star.svg" alt="">
+            <img src="assets/star.svg" alt="">
+            <img src="assets/star.svg" alt="">
+            <img src="assets/star.svg" alt="">
+        </div>
+    </div>`
+
+    document.getElementById("gpu").innerHTML+=`<div class="item">
+        <img src="assets/gpu.png" alt="">
+        <p class="name">Nombre del artículo</p>
+        <p class="price">$####</p>
+        <div class="rate">
+            <img src="assets/star.svg" alt="">
+            <img src="assets/star.svg" alt="">
+            <img src="assets/star.svg" alt="">
+            <img src="assets/star.svg" alt="">
+            <img src="assets/star.svg" alt="">
+        </div>
+    </div>`
+
+    document.getElementById("ssd").innerHTML+=`<div class="item">
+        <img src="assets/ssd.png" alt="">
+        <p class="name">Nombre del artículo</p>
+        <p class="price">$####</p>
+        <div class="rate">
+            <img src="assets/star.svg" alt="">
+            <img src="assets/star.svg" alt="">
+            <img src="assets/star.svg" alt="">
+            <img src="assets/star.svg" alt="">
+            <img src="assets/star.svg" alt="">
+        </div>
+    </div>`
+
+    document.getElementById("hdd").innerHTML+=`<div class="item">
+        <img src="assets/hdd.png" alt="">
+        <p class="name">Nombre del artículo</p>
+        <p class="price">$####</p>
+        <div class="rate">
+            <img src="assets/star.svg" alt="">
+            <img src="assets/star.svg" alt="">
+            <img src="assets/star.svg" alt="">
+            <img src="assets/star.svg" alt="">
+            <img src="assets/star.svg" alt="">
+        </div>
+    </div>`
+
+    document.getElementById("coo").innerHTML+=`<div class="item">
+        <img src="assets/cooler.png" alt="">
+        <p class="name">Nombre del artículo</p>
+        <p class="price">$####</p>
+        <div class="rate">
+            <img src="assets/star.svg" alt="">
+            <img src="assets/star.svg" alt="">
+            <img src="assets/star.svg" alt="">
+            <img src="assets/star.svg" alt="">
+            <img src="assets/star.svg" alt="">
+        </div>
+    </div>`
+
+    document.getElementById("fan").innerHTML+=`<div class="item">
+        <img src="assets/fan.png" alt="">
+        <p class="name">Nombre del artículo</p>
+        <p class="price">$####</p>
+        <div class="rate">
+            <img src="assets/star.svg" alt="">
+            <img src="assets/star.svg" alt="">
+            <img src="assets/star.svg" alt="">
+            <img src="assets/star.svg" alt="">
+            <img src="assets/star.svg" alt="">
+        </div>
+    </div>`
+
+    document.getElementById("psu").innerHTML+=`<div class="item">
+        <img src="assets/psu.webp" alt="">
+        <p class="name">Nombre del artículo</p>
+        <p class="price">$####</p>
+        <div class="rate">
+            <img src="assets/star.svg" alt="">
+            <img src="assets/star.svg" alt="">
+            <img src="assets/star.svg" alt="">
+            <img src="assets/star.svg" alt="">
+            <img src="assets/star.svg" alt="">
+        </div>
+    </div>`
+
+    }
+})
